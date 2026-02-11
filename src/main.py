e6c52f1141e3c0254e8dd9fc62fbf9b100a4107f
jeffersonpojunior <jpereira5541@gmail.com>
2026-02-11 14:37:29 -0300

    initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# classificador quântico variacional --> aprender padrões de classificação binária em espaços de dados bidimensionais
+import pennylane as qml
+
+# data_encoding: (codificação de dados clássicos em estados quânticos através de um circuito parametrizado U(x))
+
+# Ansatz Variacional:
+
+# Medição:
+
+# Otimização de Parâmetros:
